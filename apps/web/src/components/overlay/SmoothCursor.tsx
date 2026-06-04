ae8cc505dd417b9aecb0380c263ab72b0b279e34
dhruvgugnani <gugnanidhruv@gmail.com>
2026-06-05 00:15:17 +0530

    style(cursor): increase arrow cursor size to match hand scale

--- apps/web/src/components/overlay/SmoothCursor.tsx
+++ apps/web/src/components/overlay/SmoothCursor.tsx
@@ -108,8 +108,8 @@ export function SmoothCursor() {
     >
       {cursorType === "arrow" ? (
         <svg
-          width="22"
-          height="22"
+          width="32"
+          height="32"
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
